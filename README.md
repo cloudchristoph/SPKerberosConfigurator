6bd7b60e956c738b4a1d569c6c6e3a74377c1733
Christoph Vollmann <me@cvollmann.de>
2018-05-22 20:05:01 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,62 @@
+# SharePoint Kerberos Configurator
+
+## Features
+- Creates a SPN configuration script for your farm
+- Checks for Host Named Site Collections where every used name needs an SPN
+- Checks if the URLs are registered as A records in DNS
+- Checks for non-standard ports (other than 80 and 443) due to known issues with Kerberos
+- Also adds SPNs for other services like 
+    - C2WTS (Claims to Windows Token Service)
+
+## Usage
+
+There ~~are~~ will be two ways to use this script. The easiest is the interactive mode where this script goes through your Web Applications and gives you a script for correctly configuring your environment.
+
+### Interactive mode (recommended)
+
+1. Run a PowerShell on one of your SharePoint Servers as a Farm Administrator
+
+    .\SPKerberosConfigurator.ps1
+
+2. You'll get a configuration script that your Domain Administrator can run to set the SPN entries for your farm (by default this will be "ConfigureSPN.ps1")
+
+### Script mode
+
+_not implemented yet_
+
+### Parameters
+
+| Parameter                    | Type        | Description                                                 | Default          | Allowed Values                                |
+| ---------------------------- | ----------- | ----------------------------------------------------------- | ---------------- | --------------------------------------------- |
+| Step                         | ValidateSet | Tells the script which step to run                          | -                | TestSPConfig, TestSPNEntries, CreateSPNScript |
+| FileName                     | String      | The name for the SPN script or CSV file                     | ConfigureSPN.ps1 |                                               |
+| IncludeCentralAdministration | Bool        | Will also create SPN entries for the Central Administration | $false           | $true, $false                                 |
+| OutputFormat                 | ValidateSet | Sets the desired output format                              | Script           | Script, CSV                                   |
+
+
+## Limitations and Known Issues
+
+- Only works for Accounts in the same domain as the SharePoint Servers
+- Not tested against SharePoint 2010 yet
+- Alternate Access Mappings are not considered
+- Claims to Windows Token Service delegation for SQL service SPNs are not added
+
+
+## Manual steps
+This script does not automate the whole process. There are some manual steps to do after running the configuration script
+- If the Claims to Windows Token Service is used, you have to trust the corresponding service account for delegation of the SQL Service (see: https://support.microsoft.com/en-us/help/2722087/how-to-configure-claim-to-windows-token-services-in-sharepoint-2010-wi)
+
+## Next steps
+
+I'll implement the following features in the future
+
+- [ ] Checking the current Kerberos configuration for Web Applications
+- [ ] Checking if neccessary Kerberos SPNs are correctly set or already registered
+- [ ] Adding SPNs for other services like 
+    - [ ] Excel Services (only for SharePoint 2010 and SharePoint 2013 because it's integrated in Office Online Server for SharePoint 2016)
+    - [ ] Reporting Services
+    - [ ] PowerPivot
+    - [ ] PerformancePoint Services
+- [ ] Creating an SPN for your App / Add-in domain
+- [ ] Running the created SPN configuration with provided Domain Admin credentials
+
---
+++ SPKerberosConfigurator.ps1
@@ -0,0 +1,170 @@
+param(
+    # Filename for exported SPN configuration script
+    [Parameter()]
+    [string]
+    $FileName = "ConfigureSPN.ps1",
+
+    # Step to run
+    [Parameter()]
+    [ValidateSet("TestSPConfig", "TestSPNEntries", "CreateSPNScript")]
+    $Step = $null,
+
+    # IncludeCentralAdministration
+    [Parameter()]
+    [bool]
+    $IncludeCentralAdministration = $false,
+
+    [Parameter()]
+    [ValidateSet("Script", "CSV")]
+    $OutputFormat = "Script"
+
+)
+
+Set-StrictMode -Version Latest
+
+function CreateSpnEntry {
+    param(
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+        [string]$ServiceClass = "HTTP",
+		[Parameter()]
+		[ValidateNotNullOrEmpty()]
+        [string]$Hostname = "",
+        [string]$Port = "",
+		[Parameter(Mandatory=$true)]
+        [string]$Username
+    )
+
+    $SpnString = $ServiceClass + "/" + $Hostname
+    if (-not [string]::IsNullOrEmpty($Port) -and $Port -ne 80 -and $Port -ne 443) {
+        $SpnString += ":" + $Port
+    }
+
+    return @{
+        UserName = $Username
+        SpnString = $SpnString
+    }
+}
+
+function GetNonHostHeaderWebApp {
+    # TODO - find Binding with no host header other than CA
+    
+    $webApplications = Get-SPWebApplication
+    foreach ($webApplication in $webApplications) {
+        write-host "webapp $($webApplication.Url)"
+        foreach ($settings in $webApplication.IisSettings) {
+            foreach ($binding in $settings.ServerBindings) {
+                if ($binding.HostHeader -eq "") {
+                    return $webApplication
+                }
+            }
+        }
+    }
+    
+    #$webapp.IisSettings[0].ServerBindings
+    
+}
+
+function FindSPN {
+    # TODO - Filter account for SPN
+
+    $search = New-Object DirectoryServices.DirectorySearcher([ADSI]"")
+    $search.filter = "(servicePrincipalName=*)"
+    $results = $search.Findall()
+    $results
+
+}
+
+try {
+
+    Add-PSSnapIn Microsoft.SharePoint.PowerShell
+    Import-Module ($PSScriptRoot + "\ActiveDirectorySPN.psm1")
+
+    [System.Collections.ArrayList]$SpnCollection = @()
+
+    Write-Host "Trying to get the SharePoint Farm..."
+    $farm = Get-SPFarm -ErrorAction Stop
+    $SharePointVersion = $farm.BuildVersion.Major
+
+    Write-Host "Getting Web Applications..."
+    if ($IncludeCentralAdministration) {
+        $webApplications = Get-SPWebApplication -IncludeCentralAdministration -ErrorAction Stop
+    } else {
+        $webApplications = Get-SPWebApplication -ErrorAction Stop
+    }
+
+    foreach ($webApplication in $webApplications) {
+
+        # Is central admin?
+        #if ($webApplication.IsAdministrationWebApplication) {
+        #}
+
+        $Url = [System.Uri]$webApplication.Url
+        write-host $Url
+
+        # Port other that 80 or 443 - not recommended
+        if ($Url.Port -ne 80 -and $Url.Port -ne 443) {
+            Write-Warning ("The Web Application '" + $webApplication.DisplayName + "' uses a port other than 80 or 443; There are known problems with Kerberos if you're using non-standard ports.")
+        }
+
+        # DNS is registered as A records?
+        write-host "Checking DNS entry for $($Url.Host)..."
+        $dns = Resolve-DnsName -Name $Url.Host -NoHostsFile -Type All -ErrorAction SilentlyContinue
+        if ($null -ne $dns) {
+            if ($dns.Type -ne "A") {
+                Write-Warning ("The URL " + $Url.Host + " is registered with a " + $dns.Type + " record. Consider changing this entry to an A record because there are known issues (see: https://technet.microsoft.com/en-us/library/gg502606(v=office.14).aspx)")
+            }
+        } else {
+            Write-Warning ("The URL " + $Url.Host + " seems to have no DNS record. Consider adding an A record.")
+        }
+
+        # Add to collection
+        $SpnCollection.Add((CreateSpnEntry -Hostname $Url.Host -Port $Url.Port -Username $webApplication.ApplicationPool.Username));        
+
+    }
+
+    # AppDomain - get WebApp w/o HostHeader Binding
+    # C2WTS
+    Get-SPServiceInstance | Where-Object { $_.TypeName -eq "Claims to Windows Token Service" } | ForEach-Object {
+        if ($_.Status -ne "Disabled") {
+            $SpnCollection.Add((CreateSpnEntry -ServiceClass "SP" -Hostname "C2WTS" -Username $_.Service.ProcessIdentity.Username));            
+            return
+        }
+    }
+
+    # Excel Services - only for SP 2010 and 2013, because it is integrated in OOS for SP 2016
+    if ($SharePointVersion -lt 16) {
+    }
+    # Reporting Services
+    # PowerPivot
+    # PerformancePoint Services
+
+    # Host Named Site Collections
+    write-host "Getting Host Named Site Collections..."
+    Get-SPSite -Limit All | Where-Object { $_.HostHeaderIsSiteName -eq $true} | ForEach-Object {
+
+        if ($_.IsSiteMaster) {
+            return
+        }
+        $Url = [System.Uri]$_.Url
+        $SpnCollection.Add((CreateSpnEntry -Hostname $Url.Host -Port $Url.Port -Username $_.WebApplication.ApplicationPool.Username));
+    }
+
+    if ($OutputFormat -eq "Script") {
+        "# Script for configuring SPN" | Out-File -FilePath $FileName
+        foreach ($entry in $SpnCollection) {
+            ("setspn -S " + $entry["SPNString"] + " " + $entry["UserName"]) | Out-File -FilePath $FileName -Append
+        }
+    } elseif ($OutputFormat -eq "CSV") {
+        $SpnCollection | Export-Csv -Path $FileName -NoTypeInformation -Delimiter ";"
+    }
+
+
+}
+catch {
+    $ErrorMessage = $_.Exception.Message
+    $FailedItem = $_.Exception.ItemName
+
+    Write-Error $ErrorMessage
+    Write-Error $FailedItem
+}
